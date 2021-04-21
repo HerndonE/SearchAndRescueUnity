9411e13ee9cf4c7c2de5f7d7a0eb8908c778846b
HerndonE <EHerndon17@gmail.com>
2021-04-20 21:39:22 -0700

    [update] cleared for landing

---
+++ Unity/AR Demo_clone/Assets/Ethan/Scripts/rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(-90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
