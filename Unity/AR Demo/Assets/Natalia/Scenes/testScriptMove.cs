08ecaad205dc67d29364dc4a018624cf7a627d3d
sngillca <sngill.ca@gmail.com>
2021-04-22 18:47:19 -0700

    committing new branch

---
+++ Unity/AR Demo/Assets/Natalia/Scenes/testScriptMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScriptMove : MonoBehaviour
+{
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer() 
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1f);
+        cc.SimpleMove(move*5f);
+    }
+}
