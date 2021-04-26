229ca0f4bb32801bff843d5a0379bf426bf14cbe
sngillca <sngill.ca@gmail.com>
2021-04-25 19:05:47 -0700

    mlapi check

--- Unity/AR Demo/Assets/Natalia/Scenes/MenuScript.cs
+++ Unity/AR Demo/Assets/Natalia/Scenes/MenuScript.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Transports.UNET;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
     public GameObject menuPanel; // switch on and off
+    public InputField InputField;
 
     
     public void Host()
@@ -16,6 +19,16 @@ public class MenuScript : MonoBehaviour
 
     public void Join()
     {
+        // clicked join -> check if empty
+        if (InputField.text.Length <= 0)
+        {
+            // error check 
+            NetworkManager.Singleton.GetComponent<UnetTransport>().ConnectAddress = "127.0.0.1";
+            
+        } else
+        {
+            NetworkManager.Singleton.GetComponent<UnetTransport>().ConnectAddress = InputField.text;
+        }
         NetworkManager.Singleton.StartClient();
         menuPanel.SetActive(false);
     }
