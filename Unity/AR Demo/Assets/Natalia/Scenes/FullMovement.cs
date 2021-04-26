531c84489df5189ccb49dccefe4c8b19e1bc4509
sngillca <sngill.ca@gmail.com>
2021-04-25 18:21:37 -0700

    networking buttons fully implemented to solo scene

--- Unity/AR Demo/Assets/Natalia/Scenes/FullMovement.cs
+++ Unity/AR Demo/Assets/Natalia/Scenes/FullMovement.cs
@@ -19,6 +19,7 @@ public class FullMovement : NetworkBehaviour
         } else
         {
             cc = GetComponent<CharacterController>();
+            Cursor.lockState = CursorLockMode.Locked;
         }
         
     }
--- Unity/AR Demo/Assets/Natalia/Scenes/MenuScript.cs
+++ Unity/AR Demo/Assets/Natalia/Scenes/MenuScript.cs
@@ -6,15 +6,18 @@ using MLAPI;
 public class MenuScript : MonoBehaviour
 {
     public GameObject menuPanel; // switch on and off
+
     
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
+        menuPanel.SetActive(false);
     }
 
     public void Join()
     {
         NetworkManager.Singleton.StartClient();
+        menuPanel.SetActive(false);
     }
     
 } // class
