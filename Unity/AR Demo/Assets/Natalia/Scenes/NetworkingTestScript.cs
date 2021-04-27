2e5049c90d0ed3f44fe2e50d2ae43979b97782a7
sngillca <sngill.ca@gmail.com>
2021-04-26 20:47:41 -0700

    2021 networkin code

--- Unity/AR Demo/Assets/Natalia/Scenes/NetworkingTestScript.cs
+++ Unity/AR Demo/Assets/Natalia/Scenes/NetworkingTestScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Transports.UNET;
 
 public class NetworkingTestScript : NetworkBehaviour
 {
