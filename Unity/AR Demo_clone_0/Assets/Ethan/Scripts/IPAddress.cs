06a8de20fd783b275b6183c88ccc2a306946d639
Riley Galloway <rgalloway@csumb.edu>
2021-05-04 14:54:27 -0700

    Trouble shooting.
    52180 changes occurred just by opening the project. This is a tests to see what those files effect without discarding all of them.

---
+++ Unity/AR Demo_clone_0/Assets/Ethan/Scripts/IPAddress.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+using System.Net;
+using UnityEngine.UI;
+
+public class IPAddress : MonoBehaviour
+{
+
+    public Text ipAddressText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        string hostname = Dns.GetHostName();
+        IPHostEntry host = Dns.GetHostEntry(hostname);
+
+        Debug.Log($"Host: {hostname}");
+
+
+        int count = 0;
+        foreach (System.Net.IPAddress address in host.AddressList)
+        {
+            count++;
+            //Debug.Log($"    {address}");
+            if (count == host.AddressList.Length)
+            {
+                Debug.Log($"IPv4 Address: {address}");
+                ipAddressText.text = ""+address;
+            }
+        }
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+}
---
+++ Unity/AR Demo_clone_1/Assets/Natalia/Scenes/NetworkingTestScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Transports.UNET;
+
+public class NetworkingTestScript : NetworkBehaviour
+{
+    CharacterController cc;
+    public Transform cameraTransform;
+    float pitch = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (!IsLocalPlayer) 
+        {
+            cameraTransform.GetComponent<AudioListener>().enabled = false;
+            cameraTransform.GetComponent<Camera>().enabled = false;
+        } else
+        {
+            cc = GetComponent<CharacterController>();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsLocalPlayer) 
+        {   // do moving stuff
+            MovePlayer();
+            Look();
+        }
+    }
+
+    void MovePlayer() 
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1f);
+        move = transform.TransformDirection(move);
+        cc.SimpleMove(move*5f);
+    }
+
+    void Look()
+    {
+        float mousex = Input.GetAxis("Mouse X")*3f;
+        transform.Rotate(0, mousex, 0);
+        pitch -= Input.GetAxis("Mouse Y")*3f;
+        pitch = Mathf.Clamp(pitch, -45f, 45f);
+        cameraTransform.localRotation = Quaternion.Euler(pitch,0,0);
+    }
+}
